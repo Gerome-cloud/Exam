91c757337a75f00307eb2f2d6b5dbd9b7f770889
Gerome-cloud <qgdlfernandez@tip.edu.ph>
2024-12-12 22:34:12 +0800

    weather.py

--- weather.py
+++ weather.py
@@ -1,35 +1,32 @@
 import streamlit as st
 import tensorflow as tf
-from PIL import Image, ImageOps
-import numpy as np
 
-
-@st.cache_resource
+@st.cache(allow_output_mutation=True)
 def load_model():
-    model = tf.keras.models.load_model('Recog.keras')
-    return model
-
-model = load_model()
-
+  model=tf.keras.models.load_model('/content/Recog.keras')
+  return model
+model=load_model()
 st.write("""
-# Weather Recognition System
-""")
-file = st.file_uploader("Choose a weather photo from your computer", type=["jpg", "png"])
+# Weather """
+)
+file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
 
-def import_and_predict(image_data, model):
-    size = (64, 64)
-    image = image_data.resize(size, Image.Resampling.LANCZOS)
-    img = np.asarray(image)
-    img_reshape = np.expand_dims(img, axis=0)
-    prediction = model.predict(img_reshape)
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(64,64)
+    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
     return prediction
-
 if file is None:
     st.text("Please upload an image file")
 else:
-    image = Image.open(file)
-    st.image(image, use_container_width=True)
-    prediction = import_and_predict(image, model)
-    class_names = ['Cloudy', 'Rain', 'Shine', 'Sunrise']
-    result = f"OUTPUT: {class_names[np.argmax(prediction)]}"
-    st.success(result)
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['Cloudy', 'Rain', 'Shine', 'Sunrise']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
